b20f19a1535c3eeafd8eeb21f002febaeca79ece
aml00074 <92088859+aml00074@users.noreply.github.com>
2022-12-01 16:11:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import subprocess
 import RPi.GPIO as GPIO
+import time
 
 def cpu_temp():
 	thermal_zone = subprocess.Popen(['cat', '/sys/class/thermal/thermal_zone0/temp'], stdout=subprocess.PIPE)
@@ -8,25 +9,16 @@ def cpu_temp():
 	cpu_temp = int(out.decode())/1000
 	return cpu_temp
 
-def gpu_temp():
-	measure_temp = subprocess.Popen([', que '], stdout=subprocess.PIPE)
-	out, err = measure_temp.communicate()
-	gpu_temp = out.decode().split('=')[1].split('\'')[0]
-	return gpu_temp
-
-
 def check_temp():
 	cpu = cpu_temp()
-	gpu = gpu_temp()
-	if (float(cpu) > 45 or float(gpu) > 45) and not GPIO.input(7):
-		GPIO.output(14, True)
-	elif float(cpu) <= 40 and float(gpu) <= 40 and GPIO.input(7):
-		GPIO.output(14, False)
+	if (float(cpu) > 49) and not GPIO.input(18):
+		GPIO.output(18, True)
+	elif float(cpu) <= 42  and GPIO.input(18):
+		GPIO.output(18, False)
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(14,GPIO.OUT)
-check_temp()
-
-print ("CPU: "+str(cpu_temp())+"º")
-print ("GPU: "+str(gpu_temp())+"º")
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18,GPIO.OUT)
+while(1):	
+	check_temp()
+	time.sleep(5)
