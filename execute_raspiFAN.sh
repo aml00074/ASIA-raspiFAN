06950abc21384cffbda84ea615d31e77a1d1ab4d
Josse <llopez@ujaen.es>
2023-02-27 00:22:18 +0100

    updated sh

---
+++ execute_raspiFAN.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /home/pi/miniforge3/bin/activate  /home/pi/miniforge3/envs/node
+cd /home/pi/ASIA-raspiFAN
+python main.py
--- execute_raspiFan.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source /home/pi/miniconda3/bin/activate  /home/pi/miniconda3/envs/node
-cd /home/pi/ASIA-raspiFan
-python main.py
